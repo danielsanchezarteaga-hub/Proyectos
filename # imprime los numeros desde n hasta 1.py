0c665db85abaf3733ea816d3aef2a563b1f591a7
Dani-U <daniel.sanchez.arteaga@correounivalle.edu.co>
2026-07-30 13:56:26 -0500

    Delete # imprime los numeros desde n hasta 1.py

--- # imprime los numeros desde n hasta 1.py
+++
@@ -1,9 +0,0 @@
-# imprime los numeros desde n hasta 1 
-def imprimir_numeros(n):
-    if n < 1:
-        return 1
-    else: 
-        print(n)
-        return imprimir_numeros(n - 1)
-
-imprimir_numeros(5)
